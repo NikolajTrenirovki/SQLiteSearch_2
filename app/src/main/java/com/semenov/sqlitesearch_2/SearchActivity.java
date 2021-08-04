f2dff65f3d24d62c0cf1fe0815285def33941866
NikolajTrenirovki <nikolya.semenov.1997@list.ru>
2021-08-05 00:19:02 +0300

    add cod in searchActivity

--- app/src/main/java/com/semenov/sqlitesearch_2/SearchActivity.java
+++ app/src/main/java/com/semenov/sqlitesearch_2/SearchActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ public class SearchActivity extends AppCompatActivity {
 
     DatabaseHandler databaseHandler;
     RecyclerView recyclerView;
+    EditText editText;
 /*    TextView textView;
     TextView textView2;*/
 
@@ -26,13 +28,15 @@ public class SearchActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerView2);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        editText = (EditText) findViewById(R.id.editText);
+
 /*        textView = (TextView) findViewById(R.id.textView3);
         textView2 = (TextView) findViewById(R.id.textView4);*/
 
         ArrayList<Contact> contacts = new ArrayList<>();
-        contacts.add(databaseHandler.getContact("Имя"));
+        contacts.add(databaseHandler.getContact("Имя")); // add text from edittext
 
-        MainAdapter mainAdapter = new MainAdapter(contacts);
+        MainAdapter mainAdapter = new MainAdapter(contacts); 
         recyclerView.setAdapter(mainAdapter);
     }
 }
