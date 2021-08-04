c14c108108207e041d2cd635713118ce5c898595
NikolajTrenirovki <nikolya.semenov.1997@list.ru>
2021-08-04 15:38:26 +0300

    new

--- app/src/main/java/com/semenov/sqlitesearch_2/MainActivity.java
+++ app/src/main/java/com/semenov/sqlitesearch_2/MainActivity.java
@@ -1,14 +1,33 @@
 package com.semenov.sqlitesearch_2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ArrayList <Contact> contacts = new ArrayList<>();
+
+        contacts.add(new Contact("Имя", "номер"));
+        contacts.add(new Contact("Имя", "номер"));
+        contacts.add(new Contact("Имя", "номер"));
+        contacts.add(new Contact("Имя", "номер"));
+
+        MainAdapter mainAdapter = new MainAdapter(contacts);
+        recyclerView.setAdapter(mainAdapter);
     }
 }
---
+++ app/src/main/java/com/semenov/sqlitesearch_2/MainAdapter.java
@@ -0,0 +1,51 @@
+package com.semenov.sqlitesearch_2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder> {
+
+    ArrayList <Contact> contactArrayList;
+
+    public MainAdapter(ArrayList<Contact> contactArrayList) {
+        this.contactArrayList = contactArrayList;
+    }
+
+    @NonNull
+    @Override
+    public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new MainViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+        holder.name.setText(contactArrayList.get(position).getName());
+        holder.phoneNumber.setText(contactArrayList.get(position).getPhoneNumber());
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactArrayList.size();
+    }
+
+    public class MainViewHolder extends RecyclerView.ViewHolder {
+
+        TextView name;
+        TextView phoneNumber;
+
+        public MainViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name = (TextView) itemView.findViewById(R.id.textView);
+            phoneNumber = (TextView) itemView.findViewById(R.id.textView2);
+        }
+    }
+}
